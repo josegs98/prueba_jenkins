b4d42bb6ccbeb5628346ab628e5343a3828d980b
josegs98 <josegsosa98@gmail.com>
2018-03-21 16:14:19 +0100

    Update PruebaJenkins.java

--- src/pruebajenkins/PruebaJenkins.java
+++ src/pruebajenkins/PruebaJenkins.java
@@ -17,7 +17,7 @@ public class PruebaJenkins {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hola mundo");
+        System.out.println("Nada");
         
         while (true){
             
